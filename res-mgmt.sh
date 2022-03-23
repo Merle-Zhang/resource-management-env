58a4b5b28e2b60a1166a22b07d13b283783f7de8
Merle Zhang <merle.xzhang@gmail.com>
2022-03-23 17:25:37 +0000

    job script

---
+++ res-mgmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name res-mgmt
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --time 00:05:00
+#SBATCH --partition veryshort
+#SBATCH --output res-mgmt.out
+
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Slurm job ID is $SLURM_JOB_ID
+echo This job runs on the following machines:
+echo `echo $SLURM_JOB_NODELIST | uniq`
+
+#! Run the executable
+
+~/.conda/envs/res-mgmt-rl-dev/bin/python3 testrun.py
